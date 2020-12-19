5cae7b91a843b13a966445afa22d8c54bef3b606
Manpreet Singh <manpreetbassi96@gmail.com>
2020-12-19 02:51:56 -0800

    Update motor_driver.ino

--- arduino/motor_driver.ino
+++ arduino/motor_driver.ino
@@ -1,21 +1,63 @@
-#include "Wire.h"
+#include <Arduino.h>
+#include <Wire.h>
 
-void setup()
-{
-    Wire.begin(1);
-    Wire.onReceive(receiveEvent);
+#include "motor_driver.h"
 
+// State machine modes
+const enum State{READ, RESPOND, EXECUTE};
+
+// Valid instructions
+// const enum Instruction{};
+
+const int ledPin = 13; // onboard LED
+static_assert(LOW == 0, "Expecting LOW to be 0");
+
+bool parity;
+uint8_t rcvdMssg;
+
+void receiveEvent(int);
+void requestEvent();
+bool getParity(unsigned int);
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin(0x8);                // join i2c bus with address #8
+    Wire.onReceive(receiveEvent); // register event
+    Wire.onRequest(requestEvent);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, LOW); // turn it off
 }
 
-void loop()
-{
-
+void loop() {
+    delay(100);
 }
 
-void receiveEvent( int i)
-{
-    while (1 < Wire.available())
-    {
-        
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int foo) {
+    while (Wire.available()) { // loop through all but the last
+        uint8_t c = Wire.read(); // receive byte as a unsigned 8 bit integer
+        parity = getParity(c); // Calculate parity on recive
+        Serial.print("Rx: ");
+        Serial.print(c, HEX);
+        Serial.print("\tP: ");
+        Serial.println(parity);
+        rcvdMssg = c;
     }
+}
+
+void requestEvent()
+{
+    Wire.write(parity);
+}
+
+bool getParity(unsigned int n) 
+{ 
+    bool parity = 0; 
+    while (n) 
+    { 
+        parity = !parity; 
+        n      = n & (n - 1); 
+    }         
+    return parity; 
 }
