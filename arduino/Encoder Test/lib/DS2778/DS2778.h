f12f145806fe7820189bd9a1b9fcf5dbbe2b5563
manpreet-singh <manpreetbassi96@gmail.com>
2022-07-27 16:21:23 -0700

    Some more progress on PCB layout, More work on C/C++ Driver

--- arduino/Encoder Test/lib/DS2778/DS2778.h
+++ arduino/Encoder Test/lib/DS2778/DS2778.h
@@ -55,4 +55,78 @@
 
 #define ADDR                0x80 // i2c Address (R/W). This is how to change i2c address of IC
 
+// Struct to be able to hold all of the information available from the DS2778
+struct DS2778
+{
+    uint8_t     protection;
+    uint8_t     status;
+    uint16_t    active_capacity;
+    uint16_t    standby_capacity;
+    uint8_t     active_capacity_percent;
+    uint8_t     standby_capacity_percent;
+    uint16_t    avg_current;
+    uint16_t    temperature;
+    uint16_t    lower_cell_volts;
+    uint16_t    inst_current;
+    uint32_t    acc_current;
+    uint8_t     pack_age;
+    uint16_t    active_empty;
+    uint16_t    standby_empty;
+    uint16_t    high_cell_volts;
+    uint8_t     carge_cycle_count;
+} battery_pack;
+
+#ifdef __AVR__ // If we're using some AVR Device
+#include <Arduino.h> // Not guarateed to work on ALL AVR devices
+#include <Wire.h>
+
+//Temporary
+uint8_t device_addr = 0x59;
+
+/**
+ * @brief Read 1 byte of data from a specific register
+ * 
+ * @param i2c Pointer to Wire object to use for transmission.
+ * @param reg_addr Address of the register to read from
+ * @return uint8_t Data
+ */
+uint8_t read_register_8bits(TwoWire *i2c, uint8_t reg_addr)
+{
+    i2c->beginTransmission(device_addr);    // Start Transmission
+    i2c->write(reg_addr);                   // Tell device which register to read
+    i2c->requestFrom(device_addr, 1);       // Request 1 Byte (8 bits) from device
+    uint8_t data = i2c->read();             // Read data from device
+    i2c->endTransmission();                 // End Transmission once finished
+    return data;
+}
+
+/**
+ * @brief Read 2 bytes of data from 2 registers, and join them into a single 2 byte variable
+ * 
+ * @param i2c Pointer to Wire object to use for transmission.
+ * @param msb_addr Address of the MSB register
+ * @param lsb_addr Address of the LSB register
+ * @return uint16_t Data
+ */
+uint16_t read_2_registers_16bits(TwoWire *i2c, uint8_t msb_addr, uint8_t lsb_addr)
+{
+    i2c->beginTransmission(device_addr);
+
+    i2c->write(msb_addr);
+    i2c->requestFrom(device_addr, 1);
+    uint8_t msb = i2c->read();
+
+    i2c->write(lsb_addr);
+    i2c->requestFrom(device_addr, 1);
+    uint8_t lsb = i2c->read();
+
+    i2c->endTransmission();
+
+    uint16_t data = ((msb << 8) | lsb);
+
+    return data;
+}
+
+#endif // __AVR__
+
 #endif //DS2778_H
