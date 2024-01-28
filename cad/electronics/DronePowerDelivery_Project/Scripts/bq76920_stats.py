cdb669d95f9929a32062a3a0a3964ae6c2c2ddd1
manpreet-singh <manpreetbassi96@gmail.com>
2024-01-27 22:36:35 -0800

    Basic Initialization script to enable discharging

--- cad/electronics/DronePowerDelivery_Project/Scripts/bq76920_stats.py
+++ cad/electronics/DronePowerDelivery_Project/Scripts/bq76920_stats.py
@@ -18,28 +18,23 @@ prpl = port.get_port(device)
 
 bat_volts = 0
 
+## Device Initalization
 prpl.write([CC_CFG, 0x19], relax=False)
-# prpl.write([SYS_STAT, 0x80], relax=False)
 
+## 
+prpl.write([SYS_CTRL1, 0x18]) # Switch external thermister and ADC on
+prpl.write([SYS_CTRL2, 0x40]) # switch CC_EN on
+
+
+sys_ctrl2 = prpl.exchange([SYS_CTRL2], 1) # Read back SYS_CTRL2 Register
+sys_ctrl2_int = int.from_bytes(sys_ctrl2, byteorder='big')
+prpl.write([SYS_CTRL2, sys_ctrl2_int | 0b00000010]) # Enable Discharging battery
 sleep(1) # Sleep for 1 second
 
 while True:
-    # prpl.write([VC1_HI], relax=False)
-    # device_response = bytes(prpl.read(1))
-
-    # bat_volts = int.from_bytes(device_response, 'big')
-    # bat_volts = bat_volts << 8
-
-    # prpl.write([VC1_LO], relax=False)
-    # device_response = bytes(prpl.read(1))
-    # bat_volts = bat_volts + int.from_bytes(device_response, 'big')
-    # bat_volts = bat_volts & 0xFF
-
-    # print(bat_volts*0.380 + 30)
-    # print(device_response)
 
     print()
-    out = prpl.exchange([SYS_CTRL1], 1)
+    out = prpl.exchange([SYS_STAT], 1)
     print(out)
     sleep(1)
     out = prpl.exchange([SYS_CTRL2], 1)
